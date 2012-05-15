21b6fc00c35a078acc7623fc13ba0e9e1d1b7a9e
Ben Griffiths <benjamin.griffiths@nokia.com>
2012-05-15 15:47:14 +0100

    Added a simple toggle button to forms.html

--- app/js/form-controller.js
+++ app/js/form-controller.js
@@ -1,4 +1,10 @@
 function FormController($scope) {
     $scope.name = "name";
     $scope.age = 23;
+
+    $scope.state = true;
+
+    $scope.updateState = function(newState) {
+        $scope.state = newState;
+    };
 }
