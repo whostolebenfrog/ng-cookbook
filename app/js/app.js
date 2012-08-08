daaa09f75e9808f2461f22a4dc5eb90f39acab1f
Ben Griffiths <benjamin.griffiths@nokia.com>
2012-08-08 11:54:25 +0100

    Added modal dialog toggled via url path

--- app/js/app.js
+++ app/js/app.js
@@ -5,5 +5,7 @@ angular.module('cookbook', ['cookbook.filters', 'cookbook.services', 'cookbook.d
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/form', {template: 'partials/form.html', controller: FormController});
     $routeProvider.when('/view2', {template: 'partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.when('/modal-dialog/:detail', {template: 'partials/modal-dialog.html', controller: ModalDialog});
+    $routeProvider.when('/modal-dialog', {template: 'partials/modal-dialog.html', controller: ModalDialog});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,9 +4,15 @@
 
 
 function MyCtrl1() {}
-MyCtrl1.$inject = [];
 
+function MyCtrl2() {}
 
-function MyCtrl2() {
+function ModalDialog($scope, $routeParams) {
+
+    $scope.detailId = $routeParams.detail;
+
+    $scope.hideModal = function() {
+        return $scope.detailId == undefined;
+    }
+
 }
-MyCtrl2.$inject = [];
